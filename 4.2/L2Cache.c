e531278b6ae789da61be431a68592c4b1b7cc8cc
hiperbolt <tomasprsimoes@gmail.com>
2024-09-20 17:58:01 +0100

    initial work on 4.2

--- 4.2/L2Cache.c
+++ 4.2/L2Cache.c
@@ -1,5 +1,36 @@
-#include "L1Cache.h"
+#include "L1Cache.h" // Use previous implementation of L1Cache
 #include "L2Cache.h"
+
+uint8_t DRAM[DRAM_SIZE];
+uint32_t time;
+
+cache l2_cache;
+
+/**************** Time Manipulation ***************/
+void resetTime() { time = 0; }
+
+uint32_t getTime() { return time; }
+
+/****************  RAM memory (byte addressable) ***************/
+void accessDRAM(uint32_t address, uint8_t *data, uint32_t mode) {
+
+  if (address >= DRAM_SIZE - WORD_SIZE + 1)
+    exit(-1);
+
+  if (mode == MODE_READ) {
+    memcpy(data, &(DRAM[address]), BLOCK_SIZE);
+    time += DRAM_READ_TIME;
+  }
+
+  if (mode == MODE_WRITE) {
+    memcpy(&(DRAM[address]), data, BLOCK_SIZE);
+    time += DRAM_WRITE_TIME;
+  }
+}
+
+/*********************** L2 cache *************************/
+
+void initCache() { l2_cache.init = 0; }
 
 void accessL2(uint32_t address,  uint8_t *data,  uint32_t mode){
 
--- 4.2/L2Cache.h
+++ 4.2/L2Cache.h
@@ -1,8 +1,46 @@
+#ifndef L2CACHE_H
+#define L2CACHE_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+
 #include "../base_code/Cache.h"
 
+void resetTime();
+
+uint32_t getTime();
+
+/****************  RAM memory (byte addressable) ***************/
+void accessDRAM(uint32_t, uint8_t *, uint32_t);
+
+
+/*********************** Cache *************************/
+
+void initCache();
+
 void accessL1( uint32_t,  uint8_t *,  uint32_t);
+void accessL2( uint32_t,  uint8_t *,  access_mode);
+
+typedef struct cache_line {
+  uint32_t tag;
+  uint8_t data[BLOCK_SIZE];
+  uint8_t valid;
+  uint8_t dirty;
+
+} cache_line;
+
+typedef struct cache {
+  uint32_t init;
+  cache_line lines[L2_SIZE / BLOCK_SIZE];
+} cache;
+
+
+/*********************** Interfaces *************************/
+
+void read(uint32_t, uint8_t*);
+
+void write(uint32_t, uint8_t*);
+
+#endif
